ab85a33c2d275b9007201471e8f0101e050dd523
SideWorkXD <164920141+SideWorkXD@users.noreply.github.com>
2024-04-03 00:26:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,94 @@
-const { Client, GatewayIntentBits, ActivityType, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-
+const express = require('express');
 require('dotenv').config();
+const { printWatermark } = require('./functions/handlers');
+
+
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildBans,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.DirectMessageReactions,
-        GatewayIntentBits.DirectMessageTyping,
-    ],
-});
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('YaY Your Bot Status Changed✨');
-});
-app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
 });
 
-// Set the prefix for the bot
-const prefixData = require('./prefix.json');
-const prefix = prefixData.prefix;
+const prefix = '$';
+client.commands = new Map(); 
 
-client.once('ready', () => {
-    console.log('Commands Loaded successfully ❤️.');
-    console.log(`Logged in as ${client.user.username}`);
-    console.log(`Bot is in ${client.guilds.cache.size} servers`);
-    console.log(`Total members across all servers: ${client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)}`);
-    console.log('MADE BY RTX GAMING');
-    client.user.setPresence({
-        activities: [{
-            name: 'with BilloXD',
-            type: ActivityType.Playing,
-            url: 'https://www.twitch.tv/billoxd69'
-        }],
-        status: 'available'
-    });
-});
-
-
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(path.join(commandsPath, file));
-    client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.on('messageCreate', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const command = client.commands.get(commandName);
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-    if (!command) return;
+  const command = client.commands.get(commandName);
 
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('There was an error trying to execute that command!');
-    }
+  if (!command) return;
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
+});
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  const imagePath = path.join(__dirname, 'index.html');
+  res.sendFile(imagePath);
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to Billo: http://localhost:${port}`);
+});
+printWatermark();
+
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log('\x1b[32m%s\x1b[0m', '|    🍔 Bot logged in successfully!');
+    console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
+    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
+    console.log('\x1b[36m%s\x1b[0m', `|    🏡 Bot is in ${client.guilds.cache.size} servers`);
+  } catch (error) {
+    console.error('\x1b[31m%s\x1b[0m', '❌ Failed to log in:', error);
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Login, Restarting Process...');
+    process.kill(1);
+  }
+}
+
+
+client.once('ready', () => {
+  setTimeout(() => {
+    console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
+    client.user.setPresence({
+      activities: [{ name: `Type $help for Info`, type: ActivityType.Custom }],
+      status: 'dnd',
+    });
+  }, 2000); 
 });
 
 
-client.login(process.env.TOKEN);
+login();
+
+
+setInterval(() => {
+  if (!client || !client.user) {
+    console.log('\x1b[31m%s\x1b[0m', '❌ Client Not Logged in, Restarting Process...');
+    process.kill(1);
+  }
+}, 15000);
+
+module.exports = client;
