ab0abd15b831901f854f5690382d52c1a8c54d27
SideWorkXD <164920141+SideWorkXD@users.noreply.github.com>
2024-04-02 23:36:24 +0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,9 +9,9 @@ module.exports = {
 		// Create an embed object
 		const embed = new EmbedBuilder()
             .setAuthor({ name: 'MODERATION BOT' })
-            .setColor('#FFFFFF')
-            .setDescription('**HERE ARE THE AVAILABLE COMMANDS**   \n\n <a:arrow:1119532745165647902> __Basic__:\n avatar, ascii, prefix  \n\n <a:arrow:1119532745165647902>__Advanced__:\n kick, ban, serverinfo, userinfo, ping  \n\n <a:arrow:1119532745165647902> __Utility__:\n prefix, owner, support, invite  ')
-			.setThumbnail('https://cdn.discordapp.com/attachments/1111392372564168754/1119534562515624007/3696917.png')
+            .setColor('#4c0385')
+            .setDescription('**HERE ARE THE AVAILABLE COMMANDS**   \n\n <a:dstar:859462751255658506> __Basic__:\n avatar, ascii, prefix  \n\n <a:dstar:859462751255658506>__Advanced__:\n kick, ban, serverinfo, userinfo, ping  \n\n <a:dstar:859462751255658506> __Utility__:\n prefix, owner, support, invite  ')
+			.setThumbnail('https://media.discordapp.net/attachments/1149279708924629003/1222700836170502265/standard_1.gif?ex=66172be4&is=6604b6e4&hm=c53649fae3271e7a8dc5e6dea8b2edfcf1ee7f76f5e607eecc3530aed39164d9&=')
 
 		
 		
